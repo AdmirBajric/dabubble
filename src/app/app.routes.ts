c5b471c97e4d52ba9154e742e0fc2b16c22f4bc4
Admir Bajric <admir.bajric.dev@gmail.com>
2024-01-24 17:00:30 +0100

    Added Emojis Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { SendMailPwComponent } from './components/auth/send-mail-pw/send-mail-pw
 import { ResetPwComponent } from './components/auth/reset-pw/reset-pw.component';
 import { WorkspaceComponent } from './components/dashboard/workspace/workspace.component';
 import { ForTestComponent } from './for-test/for-test.component';
+import { EmojisComponent } from './components/shared/emojis/emojis.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -19,4 +20,5 @@ export const routes: Routes = [
   { path: 'impressum', component: ImpressumComponent },
   { path: 'workspace', component: WorkspaceComponent },
   { path: 'for-testing', component: ForTestComponent },
+  { path: 'emoji', component: EmojisComponent },
 ];
