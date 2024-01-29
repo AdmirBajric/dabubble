f28aa19dc9f9ecc9a61a9bb9086781c5926a892c
Selina Karlin <karlinselina@gmail.com>
2024-01-29 13:46:15 +0100

    fix: formatting for clearer view feat: showThread() / showAnswersinThread()

--- src/app/components/chat/message/message.component.ts
+++ src/app/components/chat/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -8,39 +8,45 @@ import { NgIf } from '@angular/common';
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
-export class MessageComponent implements OnInit{
+export class MessageComponent implements OnInit {
   ngOnInit(): void {
     this.TimeToStringAnswer();
   }
 
   loggedUser = "Julius Marecek"
-  @Input() message :any;
-  answersCount! :number;
-  lastAnswerTime! :string; 
+  @Input() message: any;
+  @Output() showThread = new EventEmitter<any[]>();
+  answersCount!: number;
+  lastAnswerTime!: string;
 
-getTimeFromString(dateTimeString: string): string {
+  getTimeFromString(dateTimeString: string): string {
     const dateObject = new Date(dateTimeString);
-  
+
     const stunden = dateObject.getHours();
     const minuten = dateObject.getMinutes();
-  
+
     // Führende Nullen hinzufügen, wenn die Minuten einstellig sind
     const formatierteMinuten = minuten < 10 ? '0' + minuten : minuten;
-  
+
     // Das resultierende Zeitformat ist z.B. "10:30"
     const zeitFormat = `${stunden}:${formatierteMinuten}`;
-  
+
     return zeitFormat;
   }
 
-countAnswers(){
-  this.answersCount = this.message.answers.length;
-}
+  countAnswers() {
+    this.answersCount = this.message.answers.length;
+  }
 
-TimeToStringAnswer(){
-  this.countAnswers();
-  let time = this.message.answers[this.answersCount -1].created_at;
-  this.lastAnswerTime = this.getTimeFromString(time);
+  TimeToStringAnswer() {
+    this.countAnswers();
+    let time = this.message.answers[this.answersCount - 1].created_at;
+    this.lastAnswerTime = this.getTimeFromString(time);
+  }
+
+  showAnswersinThread(answers: any[]) {
+    // console.log('message.component', answers);
+    this.showThread.emit(answers);
   }
 
 }
