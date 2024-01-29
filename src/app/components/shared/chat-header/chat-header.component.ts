33742ddb7a06b92ed2cd2f66732f278ce25d00e6
Selina Karlin <karlinselina@gmail.com>
2024-01-29 20:00:27 +0100

    feat: openEditChannel() implementing btnService

--- src/app/components/shared/chat-header/chat-header.component.ts
+++ src/app/components/shared/chat-header/chat-header.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ChannelEditComponent } from '../../chat/channel/channel-edit/channel-edit.component';
+import { ButtonFunctionService } from "../../../services/button-function.service";
 
 @Component({
   selector: 'app-chat-header',
   standalone: true,
-  imports: [],
+  imports: [ChannelEditComponent],
   templateUrl: './chat-header.component.html',
   styleUrl: './chat-header.component.scss'
 })
 export class ChatHeaderComponent implements OnInit {
   @Input() roomName!: string;
 
+  constructor(private btnService: ButtonFunctionService,){
+  }
   ngOnInit(){
+  }
 
+  openEditChannel(){
+    this.btnService.openChannelDialog();
   }
 }
