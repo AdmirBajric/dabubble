a28e638f8842fc20d1e63bd7538add9076e46473
Selina Karlin <karlinselina@gmail.com>
2024-02-08 10:57:27 +0100

    implementing more actions to component when hovering message - adding Button to edit message - sending event to parent el for editing message

--- src/app/components/shared/message-hover-actions/message-hover-actions.component.ts
+++ src/app/components/shared/message-hover-actions/message-hover-actions.component.ts
@@ -1,19 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { HoverChangeDirective } from '../../../directives/hover-change.directive';
-import {NgIf} from '@angular/common';
+import { NgIf } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
+interface Message {
+  id: string;
+  text: string;
+}
 @Component({
   selector: 'app-message-hover-actions',
   standalone: true,
-  imports: [HoverChangeDirective, NgIf],
+  imports: [HoverChangeDirective, MatTooltipModule, NgIf],
   templateUrl: './message-hover-actions.component.html',
   styleUrl: './message-hover-actions.component.scss'
 })
 export class MessageHoverActionsComponent {
   @Input() isYou!: boolean;
-  @Input() currentMessage: any;
+  @Input() currentMessage!: Message;
+  // @Output() editMessage = new EventEmitter<Message>();
+  @Output() editMessage: EventEmitter<boolean> = new EventEmitter<boolean>();
+  messageEditing!: boolean;
+  showToolTip: boolean = false;
 
-  ngOnInit(){
+  ngOnInit() {
     console.log(this.currentMessage);
   }
+
+  openEditMessage($event: MouseEvent) {
+    $event.stopPropagation();
+    this.handlingTooltip();
+    this.messageEditing = true;
+    this.editMessage.emit(this.messageEditing);
+    // this.editMessage.emit(this.currentMessage);
+  }
+
+  handlingTooltip() {
+    this.showToolTip = false;
+  }
+
 }
