7e23577ba16fc21b5936f55444a54b6e829c9fe7
Selina Karlin <karlinselina@gmail.com>
2024-02-16 17:24:56 +0100

    emitting message to be sent

--- src/app/components/shared/message-input/message-input.component.ts
+++ src/app/components/shared/message-input/message-input.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { HoverChangeDirective } from "../../../directives/hover-change.directive";
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-message-input',
   standalone: true,
-  imports: [HoverChangeDirective,MatInputModule, MatFormFieldModule, MatIconModule, MatDividerModule, MatButtonModule],
+  imports: [FormsModule, HoverChangeDirective, MatInputModule, MatFormFieldModule, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.scss'
 })
 export class MessageInputComponent {
-  
-  placeholder:string = "Nachricht an #Entwicklerteam";
+  @Output() messageText: EventEmitter<string> = new EventEmitter<string>();
+  placeholder: string = "Nachricht an #Entwicklerteam";
+  text: string = '';
 
+  emitMessage() {
+    this.messageText.emit(this.text);
+  }
 
 }
