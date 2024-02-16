64110f6cd4e5aae3f1f92bd52d2ef9477efc44a9
Selina Karlin <karlinselina@gmail.com>
2024-02-16 17:35:33 +0100

    new features: 1. saveEditedMessage() - emits the updated message text as string 2. TimeToStringAnswer() - UPDATED    - is being called only if there are answers (comments) to messages 3. getTimeFromString() - UPDATED    - variable is of type Date and not_string 4. searchForComments() - searches if message has comments stored    - if es, stores it in array named answers

--- src/app/components/chat/message/message.component.ts
+++ src/app/components/chat/message/message.component.ts
@@ -2,10 +2,12 @@ import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild }
 import { NgIf } from '@angular/common';
 import { ProfileViewComponent } from "../../profile/profile-view/profile-view.component";
 import { MessageHoverActionsComponent } from '../../shared/message-hover-actions/message-hover-actions.component';
-import { Message } from '../../../models/message.class';
+import { Comment, Message } from '../../../models/message.class';
 import { FormsModule } from '@angular/forms';
 import { HoverChangeDirective } from '../../../directives/hover-change.directive';
 import { chatNavigationService } from '../../../services/chat-navigation.service';
+import { FirebaseService } from '../../../services/firebase.service';
+
 
 @Component({
   selector: 'app-message',
@@ -15,25 +17,42 @@ import { chatNavigationService } from '../../../services/chat-navigation.service
   imports: [FormsModule, HoverChangeDirective, MessageHoverActionsComponent, NgIf, ProfileViewComponent]
 })
 export class MessageComponent implements OnInit {
-  constructor(private navService: chatNavigationService) { }
+  constructor(private navService: chatNavigationService,
+    private firebaseService: FirebaseService
+  ) { }
   loggedUser = "Selina Karlin"
   @Input() message: any;
-  @Input() showAnswers!: boolean;
-  // @Output() showThread = new EventEmitter<any[]>();
+  @Input() messageId!: string | undefined;
+  @Output() updatedMessage = new EventEmitter<{messageText: string, id: string}>();
+  showAnswers: boolean = false;
+  answers: Comment[] = [];
   answersCount!: number;
   lastAnswerTime!: string;
   showActions: boolean = false;
   openMessageEdit: boolean = false;
-  saveOriginalMessage!: string; // to reset the message text when editing is cancelled
+  saveOriginalMessage!: string;       // to reset the message text when editing is cancelled
 
-  ngOnInit(): void {
-    // console.log(this.showAnswers);s
-    if (this.showAnswers) {
-      // this.TimeToStringAnswer();
-    }
+  async ngOnInit() {
+    await this.searchForComments();
+    this.TimeToStringAnswer();
   }
 
-  getTimeFromString(dateTimeString: string): string {
+  async searchForComments() {
+    const querySnapshot = await this.firebaseService.queryDocuments(
+      'comments',
+      'messageId',
+      '==',
+      this.message.id
+    );
+    querySnapshot.forEach((doc: any) => {
+      let commentData = doc.data();
+      commentData['id'] = doc.id;
+      this.answers.push(commentData);
+      // this.editedComment = commentData['text'];
+    });
+  }
+
+  getTimeFromString(dateTimeString: Date): string {
     const dateObject = new Date(dateTimeString);
 
     const stunden = dateObject.getHours();
@@ -48,35 +67,53 @@ export class MessageComponent implements OnInit {
     return zeitFormat;
   }
 
-  // countAnswers() {
-  //   this.answersCount = this.comment.length;
-  // }
-
   TimeToStringAnswer() {
-    // this.countAnswers();
-    let time = this.message.answers[this.answersCount - 1].timestamp;
-    this.lastAnswerTime = this.getTimeFromString(time);
+    this.countAnswers();
+    if (this.answers.length > 0) {
+      let time = this.answers[this.answersCount -1].timestamp;
+      this.lastAnswerTime = this.getTimeFromString(time);
+    }
+  }
+
+  countAnswers() {
+    this.answersCount = this.answers.length;
   }
 
   showAnswersinThread(m: any[]) {
-    // this.showThread.emit(answers); old way
     this.navService.openThread(m);
   }
 
+  /**
+   * Handles the hover actions,
+   * saves the original message in case user cancels editing,
+   * opens the input field for message editing. 
+   * @param {Message} m
+   */
   editMessage(m: Message) {
-    this.handlingMessageHoverActions();
+    this.closeMessageHoverActions();
     this.saveOriginalMessage = m.text;
     this.openMessageEdit = true;
   }
 
-  handlingMessageHoverActions() {
+  closeMessageHoverActions() {
     this.showActions = false;
   }
 
-  saveEditedMessage() {
-
+  /**
+   * Emits the edited text of message to main-chat.component to be updated from there.
+   * Closes the input field for message editing.
+   * @param {string} messageText
+   * @param {string} id
+   */
+  saveEditedMessage(messageText: string, id: string) {
+      this.updatedMessage.emit({messageText, id});
+      this.openMessageEdit = false;
   }
 
+  /**
+   * Cancels the message editing by closing the input field for message editing &
+   * returns the original message text.
+   */
   cancelMessageEditing() {
     this.openMessageEdit = false;
     this.message.text = this.saveOriginalMessage;
