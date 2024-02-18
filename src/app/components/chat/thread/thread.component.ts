45af3e4e4885ce3e5e0aa261c91cfe9935416f68
Selina Karlin <karlinselina@gmail.com>
2024-02-18 16:46:20 +0100

    fix message id

--- src/app/components/chat/thread/thread.component.ts
+++ src/app/components/chat/thread/thread.component.ts
@@ -15,6 +15,9 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { chatNavigationService } from '../../../services/chat-navigation.service';
 import { MessageComponent } from "../message/message.component";
+import { User } from '../../../models/user.class';
+import { FirebaseService } from '../../../services/firebase.service';
+import { Comment } from '../../../models/message.class';
 
 @Component({
   selector: 'app-thread',
@@ -35,17 +38,18 @@ export class ThreadComponent implements OnInit {
   currentMessage!: any;
   private messageSubscription!: Subscription;
   private threadStatusSubscription!: Subscription;
-
-  loggedUser = 'Julius Marecek';
+  user!: User;
   answersCount!: number;
   mobileView!: boolean;
   windowWidth!: number;
+  answers: Comment[] = [];
 
   constructor(
     private renderer: Renderer2,
     private el: ElementRef,
     public router: Router,
-    private navService: chatNavigationService
+    private navService: chatNavigationService,
+    private firebaseService: FirebaseService
   ) { }
 
   @HostListener('window:resize', ['$event'])
@@ -71,9 +75,16 @@ export class ThreadComponent implements OnInit {
   }
 
   ngOnInit() {
+    if (typeof localStorage !== 'undefined') {
+      const user = localStorage.getItem('loggedInUser');
+      if (user) {
+        this.user = JSON.parse(user);
+      }
+    }
     this.subscribeThreadStatus();
     this.subscribeMessage();
-    // console.log('THREAD', this.currentMessage);
+    console.log('THREAD', this.currentMessage);
+    this.searchForComments();
     this.countAnswers();
   }
 
@@ -86,8 +97,25 @@ export class ThreadComponent implements OnInit {
     return zeitFormat;
   }
 
+  async searchForComments() {
+    const querySnapshot = await this.firebaseService.queryDocuments(
+      'comments',
+      'messageId',
+      '==',
+      this.threadData.id
+    );
+    querySnapshot.forEach((doc: any) => {
+      let commentData = doc.data();
+      commentData['id'] = doc.id;
+      this.answers.push(commentData);
+    });
+    console.log(this.answers);
+
+  }
+
+
   countAnswers() {
-    this.answersCount = this.threadData.answers.length;
+    this.answersCount = this.answers.length;
   }
 
   subscribeMessage() {
