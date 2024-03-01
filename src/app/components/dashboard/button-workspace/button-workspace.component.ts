5af1f85176a5b3616480c6760e1eb7dab3111aae
Selina Karlin <karlinselina@gmail.com>
2024-03-01 16:21:18 +0100

    jsdoc

--- src/app/components/dashboard/button-workspace/button-workspace.component.ts
+++ src/app/components/dashboard/button-workspace/button-workspace.component.ts
@@ -1,23 +1,31 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HoverChangeDirective } from "../../../directives/hover-change.directive";
-
+import { HoverChangeDirective } from '../../../directives/hover-change.directive';
 
 @Component({
   selector: 'app-button-workspace',
   standalone: true,
   imports: [CommonModule, HoverChangeDirective],
   templateUrl: './button-workspace.component.html',
-  styleUrl: './button-workspace.component.scss'
+  styleUrl: './button-workspace.component.scss',
 })
 export class ButtonWorkspaceComponent {
   isOpen: boolean = true;
   @Output() toggle = new EventEmitter<boolean>();
 
+  /**
+   * Returns the text to be displayed in HTML according to isOpen flag.
+   * @readonly
+   * @type {string}
+   */
   get buttonText(): string {
     return this.isOpen ? 'Workspace-Menü schließen' : 'Workspace-Menü öffnen';
   }
 
+  /**
+   * Toggles isOpen flag when clicked.
+   * Emits isOpen flag to parent (dashboard) for regulating UI
+   */
   toggleWorkspace() {
     this.isOpen = !this.isOpen;
     this.toggle.emit(this.isOpen);
